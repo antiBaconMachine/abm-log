9eb9179e619d84e2727f0dad8914ec7b22ef447c
Ollie Edwards <oliver.s.edwards@gmail.com>
2012-08-30 15:17:28 +0100

    inital dump

---
+++ abm-log.js
@@ -0,0 +1,78 @@
+var abm = abm || {};
+
+$("document").ready(function(){
+    abm.log.init("info");
+});
+
+abm.log = (function(){
+    
+    var fallback = function(msg){
+        alert(msg);
+    };
+    var suppressed = function(level) {
+        return function() {
+            return level +" suppressed";
+        }
+    }
+    var originalFunctions = [];
+    
+    var self = {
+        
+        levels : ["debug","info","warn","error"],
+    
+        init : function(minLevel) {
+            var params = abm.getUrlParams();
+            minLevel = params["debug"] || minLevel;
+        
+            //TODO: firebug lite
+            window.console = window.console || {};
+        
+            var clear = false;
+            for (var i=0; i<self.levels.length; i++) {
+                var level = self.levels[i];
+                originalFunctions[i] = originalFunctions[i] 
+                || console[level] || fallback;
+            
+                if (level && (clear || level===minLevel)) {
+                    console[level] = originalFunctions[i];
+                    clear=true;
+                } else {
+                    console[level] = suppressed(level);
+                }
+            }
+        
+        },
+        
+        reset : function() {
+            for (var i=originalFunctions.length; i>=0; i--) {
+                console[self.levels[i]] = originalFunctions[i];
+            }
+            originalFunctions = [];
+        },
+        
+        getOriginalFunctions : function() {
+            return originalFunctions;
+        }
+    };
+    return self;
+}());
+
+abm.getUrlParams = abm.getUrlParams || 
+    function() {
+        var vars = {}, hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = hashes.length-1; i >= 0; i--)
+        {
+            hash = hashes[i].split('=');
+
+            var val = hash[1];
+            var intVal = parseInt(val);
+            if (!isNaN(intVal)) {
+                val = intVal;
+            }else if (val==="true" || val==="false") {
+                val = Boolean(val);
+            }
+            vars[hash[0]] = val;
+        }
+        return vars;
+    };
---
+++ test/spec/Log.js
@@ -0,0 +1,34 @@
+describe("log", function() {
+    beforeEach(function() {
+    
+    });
+    
+    it("should supress calls to console methods lower then that specified", function() {
+        abm.log.init("info");
+        expect(console.debug).not.toEqual(abm.log.getOriginalFunctions()[0]);
+        expect(console.debug()).toEqual("debug suppressed");
+    });
+    
+    it("should restore original logging functions when reinitalised", function() {
+        abm.log.init("warn");
+        expect(console.info).not.toEqual(abm.log.getOriginalFunctions()[1]);
+        abm.log.init("info");
+        expect(console.info).toEqual(abm.log.getOriginalFunctions()[1]);
+    });
+    
+    it("should return the console object to it's original state when reset is called", function() {
+        abm.log.init("error");
+        expect(console.warn()).toEqual("warn suppressed");
+        abm.log.reset();
+        expect(console.warn()).not.toEqual("warn suppressed");
+        abm.log.init("error");
+        expect(console.warn()).toEqual("warn suppressed");
+
+    });
+    
+    it("should disable all levels when a falsy value is set as the lvel", function() {
+        abm.log.init(0);
+        expect(console.error()).toEqual("error suppressed");
+    }) 
+})
+
